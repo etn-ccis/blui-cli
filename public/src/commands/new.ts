0a20cf2303f1c5e51d0d0098fda534e9a0055166
huayunh <hhuayun0828@gmail.com>
2020-04-17 17:57:28 -0400

    Implement the framework flag

--- public/src/commands/new.ts
+++ public/src/commands/new.ts
@@ -7,30 +7,44 @@ module.exports = {
     run: async (toolbox: GluegunToolbox): Promise<void> => {
         const { parse, print, createProject, addPXBlue } = toolbox;
 
-        const [framework] = await parse([
-            {
-                question: 'Project Framework:',
-                required: true,
-                type: 'radio',
-                choices: ['Angular', 'React', 'Ionic', 'React Native'],
-            },
-        ]);
+        let framework = '',
+            frameworkOptions = toolbox.parameters.options.framework;
+
+        // have the user choose a framework if they did not use the framework flag or
+        // only passed in a flag with no framework name following it.
+        if (frameworkOptions === undefined || frameworkOptions === true) {
+            [framework] = await parse([
+                {
+                    question: 'Project Framework:',
+                    required: true,
+                    type: 'radio',
+                    choices: ['Angular', 'React', 'Ionic', 'React Native'],
+                },
+            ]);
+        } else {
+            framework = frameworkOptions;
+        }
+
+        // convert the string to lower case, and remove all the spaces and dashes.
+        framework = framework.toLowerCase().replace(/[- ]/gi, '');
 
         switch (framework) {
-            case 'Angular':
+            case 'angular':
                 await addPXBlue.angular(await createProject.angular());
                 break;
-            case 'React':
+            case 'react':
                 await addPXBlue.react(await createProject.react());
                 break;
-            case 'Ionic':
+            case 'ionic':
                 await addPXBlue.ionic(await createProject.ionic());
                 break;
-            case 'React Native':
+            case 'reactnative':
                 await addPXBlue.reactNative(await createProject.reactNative());
                 break;
             default:
-                print.error('You must choose one of the supported frameworks.');
+                print.error('You must specify one of the supported frameworks.');
+                print.newline();
+                print.info('PX Blue only support Angular, React, Ionic and React Native.');
                 return;
         }
     },
